7389f74fddc74563fb230476b51290de91c1ab28
Array <shellmode13@gmail.com>
2024-06-15 14:51:14 +0800

    Update docker image to support SD3

--- comfyui_image/build_and_push.sh
+++ comfyui_image/build_and_push.sh
@@ -13,7 +13,7 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 echo "AccountID: $ACCOUNT_ID, Region: $REGION"
 
-docker build --platform="linux/amd64" . -t comfyui-images
+docker build --platform="linux/amd64" . -t comfyui-images --no-cache
 docker tag comfyui-images:latest ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/comfyui-images:latest
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
 docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/comfyui-images:latest
